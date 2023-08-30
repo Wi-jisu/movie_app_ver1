137adcd5c78c56b6886252faf44e042f76656617
Wi-jisu <namiho7@naver.com>
2023-08-30 17:49:12 +0900

    test

---
+++ src/components/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+function About(){
+    return(
+        <div className='about_container'>
+            <span>
+                개발자는 어떻게 성장해야 할까?
+                </span>
+        </div>
+    );
+}
